a3604d2e51e87f570b3903928a68c242b8105184
Sidiel Oliveira <sidiel.dev@gmail.com>
2023-11-03 19:12:45 -0300

    Preparação dos Arquivos para Inicialização do Projeto

---
+++ api-gateway/src/main/java/com/br/diel/VerbsHttpGetPostPutDeleteDielApplication.java
@@ -0,0 +1,13 @@
+package com.br.diel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerbsHttpGetPostPutDeleteDielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerbsHttpGetPostPutDeleteDielApplication.class, args);
+	}
+
+}
---
+++ api-gateway/src/test/java/com/br/diel/VerbsHttpGetPostPutDeleteDielApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.diel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerbsHttpGetPostPutDeleteDielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
