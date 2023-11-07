7cde4ad6ea168925191579a9baa50f6969787ad9
Sidiel Conceição de Oliveira <sidieloliveira@ubatuba.sp.gov.br>
2023-11-07 14:30:14 -0300

    Implementando o Padrão VO (Value Object) - Parte 1

--- api-gateway/src/main/java/com/br/diel/services/PersonServices.java
+++ api-gateway/src/main/java/com/br/diel/services/PersonServices.java
@@ -1,13 +1,14 @@
 package com.br.diel.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.br.diel.data.vo.v1.PersonVO;
 import com.br.diel.exceptions.ResourceNotFoundException;
+import com.br.diel.mapper.DozerMapper;
 import com.br.diel.model.Person;
 import com.br.diel.repositories.PersonRepository;
 
@@ -19,28 +20,30 @@ public class PersonServices {
     @Autowired
     PersonRepository repository;
 
-    public List<Person> findAll() {
+    public List<PersonVO> findAll() {
 
         logger.info("Finding all people!");
-        List<Person> persons = new ArrayList<>();
         
-        return repository.findAll();
+        return DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
     }
 
-    public Person findById(Long id) {
+    public PersonVO findById(Long id) {
 
         logger.info("Finding one person!");
         
-        return repository.findById(id)
+        var entity = repository.findById(id)
             .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+        return DozerMapper.parseObject(entity, PersonVO.class);
     }
 
-    public Person create(Person person) {
+    public PersonVO create(PersonVO person) {
         logger.info("Creating one person!");
-        return repository.save(person);
+        var entity = DozerMapper.parseObject(person, Person.class);
+        var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+        return vo;
     }
 
-    public Person update(Person person) {
+    public PersonVO update(PersonVO person) {
         logger.info("Updating one person!");
 
         var entity = repository.findById(person.getId())
@@ -51,7 +54,8 @@ public class PersonServices {
         entity.setAddress(person.getAddress());
         entity.setGender(person.getGender());
             
-        return repository.save(person);
+        var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+        return vo;
     }
 
     public void delete(Long id) {
